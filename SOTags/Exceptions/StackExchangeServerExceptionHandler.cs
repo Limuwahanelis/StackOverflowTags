2b34051c2acb520bbdf2a6a272540dded2e29a26
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-03-30 13:52:26 +0100

    Update StackExchangeServerExceptionHandler.cs

--- SOTags/Exceptions/StackExchangeServerExceptionHandler.cs
+++ SOTags/Exceptions/StackExchangeServerExceptionHandler.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+using SOTags.Controllers;
 
 namespace SOTags.Exceptions
 {
     public class StackExchangeServerExceptionHandler : IExceptionHandler
     {
+        private readonly ILogger<StackExchangeServerExceptionHandler> _logger;
+
+        public StackExchangeServerExceptionHandler(ILogger<StackExchangeServerExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
             if(exception is not StackExchangeServerCouldNotBeReachedException stackExchangeServerCouldNotBeReachedException)
             {
                 return false;
             }
-            await httpContext.Response.WriteAsJsonAsync(
-                $"An problem occured when reaching Stack Exchange server. Message from server: {stackExchangeServerCouldNotBeReachedException.StackExchangeSetverMessage}\n" +
-                $"Managed to {stackExchangeServerCouldNotBeReachedException.OperationMessage}"
-                );
+            string message = $"An problem occured when reaching Stack Exchange server. Message from server: {stackExchangeServerCouldNotBeReachedException.StackExchangeSetverMessage}\n" +
+                $"Managed to {stackExchangeServerCouldNotBeReachedException.OperationMessage}";
+            await httpContext.Response.WriteAsJsonAsync(message);
+            Log.Error("{message}",message);
             return true;
         }
     }
