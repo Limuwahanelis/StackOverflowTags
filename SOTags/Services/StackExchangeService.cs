32c758cfbfb7addc06db879a62ec6b0fba85bbc6
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-04-02 01:08:40 +0200

    Start using httpclient from DI as well as configuration from appsettings in SEService.

--- SOTags/Services/StackExchangeService.cs
+++ SOTags/Services/StackExchangeService.cs
@@ -1,52 +1,66 @@
 ﻿
 using SOTags.Exceptions;
 using SOTags.Interfaces;
+using System.Net.Http.Headers;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SOTags.Services
 {
-    public class StackExchangeService: IStackExchangeService
+    public class StackExchangeService : IStackExchangeService
     {
+        readonly string filter = "";
+
+        //private readonly IHttpClientFactory _httpClientFactory;
+        HttpClient client;
+        private readonly IConfiguration _configuration;
+
+        public StackExchangeService(IConfiguration configuration, HttpClient httpClient)
+        {
+            _configuration = configuration;
+            filter = _configuration.GetValue<string>("StackExchangeServer:Filter");
+            client = httpClient;
+        }
+
         public async Task<string> GetTagsInfoFromStackExchange(int pageSize, string tagsUrl)
         {
-            string path = $"https://api.stackexchange.com/2.3/tags/{tagsUrl}/info?pagesize={pageSize}&order=desc&sort=popular&site=stackoverflow&filter=!bMsg5CXCu9jto8";
-            string data="";
-            using (HttpResponseMessage response = await APIHelper.client.GetAsync(path))
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            string path = _configuration.GetValue<string>("StackExchangeServer:Address") + $"tags/{tagsUrl}/info?pagesize={pageSize}&order=desc&sort=popular&site=stackoverflow&filter={filter}";
+            string data = "";
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
             {
-                if (response.IsSuccessStatusCode)
+                data = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new StackExchangeServerCouldNotBeReachedException()
                 {
-                    data = await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    throw new StackExchangeServerCouldNotBeReachedException()
-                    {
-                        StackExchangeSetverMessage = await response.Content.ReadAsStringAsync()
-                    };
-                }
-                //var absPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                //data = System.IO.File.ReadAllText(absPath + "/Jsons/data.json");
+                    StackExchangeSetverMessage = await response.Content.ReadAsStringAsync()
+                };
             }
             return data;
         }
         public async Task<string> GetPagedDataFromStackExchange(int pageSize, int pageIndex)
         {
-            string path = $"https://api.stackexchange.com/2.3/tags?page={pageIndex}&pagesize={pageSize}&order=desc&sort=popular&site=stackoverflow&filter=!bMsg5CXCu9jto8";
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            string path = _configuration.GetValue<string>("StackExchangeServer:Address") + $"tags?page={pageIndex}&pagesize={pageSize}&order=desc&sort=popular&site=stackoverflow&filter={filter}";
             string data = "";
-            using (HttpResponseMessage response = await APIHelper.client.GetAsync(path))
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
             {
-                if (response.IsSuccessStatusCode)
+                data = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                throw new StackExchangeServerCouldNotBeReachedException()
                 {
-                    data = await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    throw new StackExchangeServerCouldNotBeReachedException()
-                    {
-                        StackExchangeSetverMessage = await response.Content.ReadAsStringAsync()
-                    };
-                }
-                //var absPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-                //data = System.IO.File.ReadAllText(absPath + "/Jsons/data.json");
+                    StackExchangeSetverMessage = await response.Content.ReadAsStringAsync()
+                };
             }
             return data;
         }
