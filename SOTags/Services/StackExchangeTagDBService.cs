f0603806915b234e675e42fe6aaf57fd50a9e2a4
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-04-02 01:13:17 +0200

    Load confighs in SETagDBService from appsettings

--- SOTags/Services/StackExchangeTagDBService.cs
+++ SOTags/Services/StackExchangeTagDBService.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using Microsoft.IdentityModel.Tokens;
+using Serilog;
 using SOTags.Exceptions;
 using SOTags.Interfaces;
 using SOTags.Model;
@@ -12,26 +13,31 @@ namespace SOTags.Services
     {
         private readonly ITagsRepository _tagsRepository;
         private readonly IStackExchangeService _stackExchangeService;
+        private readonly IConfiguration _configuration;
+        int _pageSize;
+        int _numberOfTagsToImport;
 
-        public StackExchangeTagDBService(ITagsRepository tagsRepository, IStackExchangeService stackExchangeService)
+        public StackExchangeTagDBService(ITagsRepository tagsRepository, IStackExchangeService stackExchangeService, IConfiguration configuration)
         {
             _tagsRepository = tagsRepository;
             _stackExchangeService = stackExchangeService;
+            _configuration = configuration;
+            _pageSize = _configuration.GetValue<int>("StackExchangeServer:PageSize");
+            _numberOfTagsToImport = _configuration.GetValue<int>("StackExchangeServer:NumberOfRequiredTags");
         }
-        public async Task ImportTagsFromStackOverflow(int numberOfTagsToImport)
+        public async Task ImportTagsFromStackOverflow()
         {
-            int pageSize = 100;
             int pageIndex = 1;
             string data = "";
-            for (int numberOfEntries = 0; numberOfEntries < numberOfTagsToImport;)
+            for (int numberOfEntries = 0; numberOfEntries < _numberOfTagsToImport;)
             {
                 try
                 {
-                    data = await _stackExchangeService.GetPagedDataFromStackExchange(pageSize, pageIndex);
+                    data = await _stackExchangeService.GetPagedDataFromStackExchange(_pageSize, pageIndex);
                 }
                 catch (StackExchangeServerCouldNotBeReachedException ex)
                 {
-                    ex.OperationMessage = $"Imported or updated {numberOfEntries} tags from required {numberOfTagsToImport} entries";
+                    ex.OperationMessage = $"Imported or updated {numberOfEntries} tags from required {_numberOfTagsToImport} entries";
                     throw;
                 }
                 List<Tag> tags = GetTagsFromStackExchangeResponse(data);
@@ -45,8 +51,6 @@ namespace SOTags.Services
 
         public async Task UpdateTagsInDB()
         {
-            int minNumberOftagsTOUpdate = 1000;
-            int pageSize = 100;
             int pageIndex = 1;
             List<Tag> tags;
             string data = "";
@@ -54,45 +58,45 @@ namespace SOTags.Services
             List<string?> allNames= new List<string?>();
             while ( numberOfTagsUpdated < _tagsRepository.GetNumberOfTagsInDB())
             {
-                List<string?> tagsNames = _tagsRepository.GetTagsName(pageSize, pageIndex-1);
+                List<string?> tagsNames = new List<string?> { "graph", "chart", "c++", "charts" }; //_tagsRepository.GetTagsName(pageSize, pageIndex-1);
                 allNames.AddRange(tagsNames);
                 string tagsToUpdate = string.Join(";", tagsNames);
                 // If tags contains some special characters it needs to be properly encoded for request to work
                 string tagsUrl = HttpUtility.UrlEncode(tagsToUpdate);
                 try
                 {
-                    data = await _stackExchangeService.GetTagsInfoFromStackExchange(pageSize, tagsUrl);
+                    data = await _stackExchangeService.GetTagsInfoFromStackExchange(_pageSize, tagsUrl);
                 }
                 catch (StackExchangeServerCouldNotBeReachedException ex)
                 {
                     ex.OperationMessage = $"Updated {numberOfTagsUpdated} tags from {_tagsRepository.GetNumberOfTagsInDB()}";
                     throw;
                 }
-                tags = GetTagsFromStackExchangeResponse(data);
+                tags = GetTagsFromStackExchangeResponse(data,tagsNames);
                 _tagsRepository.AddOrUpdateTagsToDatabase(tags);
                 numberOfTagsUpdated += tags.Count;
                 pageIndex++;
             }
             pageIndex = 1;
-            // if for some reason database lacks required 1000 tags, get the rest from stack exchange
-            if (numberOfTagsUpdated< minNumberOftagsTOUpdate)
+            // if for some reason database lacks required tags, get the rest from stack exchange
+            if (numberOfTagsUpdated < _numberOfTagsToImport)
             {
                 int numberOfEntries = numberOfTagsUpdated;
-                while (numberOfEntries < minNumberOftagsTOUpdate)
+                while (numberOfEntries < _numberOfTagsToImport)
                 {
                     try
                     {
-                        data = await _stackExchangeService.GetPagedDataFromStackExchange(pageSize, pageIndex);
+                        data = await _stackExchangeService.GetPagedDataFromStackExchange(_pageSize, pageIndex);
                     }
                     catch (StackExchangeServerCouldNotBeReachedException ex)
                     {
-                        ex.OperationMessage = $"Imported or updated {numberOfEntries} tags from required {minNumberOftagsTOUpdate} entries";
+                        ex.OperationMessage = $"Imported or updated {numberOfEntries} tags from required {_numberOfTagsToImport} entries";
                         throw;
                     }
                     tags = GetTagsFromStackExchangeResponse(data);
-                    if (numberOfEntries + tags.Count() > minNumberOftagsTOUpdate)
+                    if (numberOfEntries + tags.Count() > _numberOfTagsToImport)
                     {
-                        tags.RemoveRange(minNumberOftagsTOUpdate - numberOfEntries, tags.Count() - (minNumberOftagsTOUpdate - numberOfEntries));
+                        tags.RemoveRange(_numberOfTagsToImport - numberOfEntries, tags.Count() - (_numberOfTagsToImport - numberOfEntries));
                     }
                     //Console.WriteLine($"total tags usage = {totalNumberOfTagsUse}");
                     _tagsRepository.AddOrUpdateTagsToDatabase(tags);
@@ -113,6 +117,37 @@ namespace SOTags.Services
             foreach (JsonElement element in tagsList.EnumerateArray())
             {
                 Tag tag = element.Deserialize<Tag>();
+                tags.Add(tag);
+            }
+            return tags;
+        }
+        private List<Tag> GetTagsFromStackExchangeResponse(string data,List<string> tagNames)
+        {
+            List<Tag> tags = new List<Tag>();
+            JsonDocument jsonDocument = JsonDocument.Parse(data);
+            JsonElement tagsList = jsonDocument.RootElement.GetProperty("items");
+            foreach (JsonElement element in tagsList.EnumerateArray())
+            {
+                Tag tag = element.Deserialize<Tag>();
+                JsonElement synonyms;
+                bool hasSynonyms = element.TryGetProperty("synonyms",out synonyms);
+                if (hasSynonyms)
+                {
+                    foreach(JsonElement element1 in synonyms.EnumerateArray())
+                    {
+                        
+                        if(tagNames.Contains(element1.GetString()))
+                        {
+                            if (tag.Name.Equals(element1.GetString())) continue;
+                            Tag synonymTag = new Tag()
+                            { 
+                                Count = tag.Count,
+                                Name = element1.GetString()
+                            };
+                            tags.Add(synonymTag);
+                        }
+                    }
+                }
                 tags.Add(tag);
             }
             return tags;
