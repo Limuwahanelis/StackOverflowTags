d2af6394190ad47a09461c9f3ace882660a12a44
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-04-03 21:16:45 +0200

    Refactor TagRepository
    Add more logs. Make strings of method GetTagsName non-nullable.

--- SOTags/SOTags/Interfaces/ITagsRepository.cs
+++ SOTags/SOTags/Interfaces/ITagsRepository.cs
@@ -5,7 +5,7 @@ namespace SOTags.Interfaces
     public interface ITagsRepository
     {
         public void AddOrUpdateTagsToDatabase(List<Tag> tags);
-        public List<string?> GetTagsName(int pageSize, int pageIndex);
+        public List<string> GetTagsName(int pageSize, int pageIndex);
         public int GetNumberOfTagsInDB();
         public List<Tag> GetTagsPaged(int pageSize, int pageNumber, TagSortingHelper.TagSortingType sort, bool isDescending, out int totalCount);
         void CalculateTagsUsage();
--- SOTags/SOTags/Repositories/TagsRepository.cs
+++ SOTags/SOTags/Repositories/TagsRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using SOTags.Data;
@@ -21,23 +22,31 @@ namespace SOTags.Repositories
             foreach (Tag tag in tags)
             {
                 Tag? oldTag = _context.Tags.Where(t => t.Name == tag.Name).FirstOrDefault();
-                if (oldTag != null) UpdateTag(oldTag, tag);
-                else _context.Tags.Add(tag);
+                if (oldTag != null)
+                {
+                    if (oldTag.Count != tag.Count) UpdateTag(oldTag, tag);
+                }
+                else
+                {
+                    _context.Tags.Add(tag);
+                    Log.Information("Added new tag {tagName} with count {tagCount}", tag.Name, tag.Count);
+                }
             }
             _context.SaveChanges();
         }
         private void UpdateTag(Tag oldTag,Tag newTag)
         {
             oldTag.Name = newTag.Name;
+            Log.Information("Updated {oldtag} count from {oldCount} to {newCount}", oldTag.Name, oldTag.Count, newTag.Count);
             oldTag.Count = newTag.Count;
         }
         public int GetNumberOfTagsInDB()
         {
             return _context.Tags.Count();
         }
-        public List<string?> GetTagsName(int pageSize,int pageIndex)
+        public List<string> GetTagsName(int pageSize,int pageIndex)
         {
-            List<string?> toReturn;
+            List<string> toReturn;
             toReturn = _context.Tags.OrderByDescending(t=>t.Count).Skip(pageSize* pageIndex).Take(pageSize).Select(t=>t.Name).ToList();
             return toReturn;
         }
@@ -70,6 +79,6 @@ namespace SOTags.Repositories
             toReturn = (isDescending?_context.Tags.AsNoTracking().OrderByDescending(orderByFunc):_context.Tags.OrderBy(orderByFunc)).Skip(pageSize*(pageNumber-1)).Take(pageSize).ToList();
             return toReturn;
         }
-        Func<Tag, Object> orderByFunc = null;
+        Func<Tag, Object> orderByFunc=null;
     }
 }
